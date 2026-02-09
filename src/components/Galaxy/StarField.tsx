5f71059b85a6ef506f5a60766ef259865f5ce467
perunbro <ilariamattana22@gmail.com>
2026-02-09 21:09:24 +0100

    Update StarField.tsx

--- src/components/Galaxy/StarField.tsx
+++ src/components/Galaxy/StarField.tsx
@@ -1,34 +1,36 @@
 'use client';
 
-import { useEffect, useState, memo } from 'react';
+import { useEffect, memo } from 'react';
 import { useGalaxyStore } from '@/store/useGalaxyStore';
 
 function Star({ secret }: any) {
-  const openSecret = useGalaxyStore(s => s.openSecret);
+  const selectSecret = useGalaxyStore(s => s.selectSecret);
 
   return (
     <mesh
       position={secret.position}
       onPointerDown={(e) => {
         e.stopPropagation(); // 🔴 FONDAMENTALE
-        openSecret(secret);
+        selectSecret(secret);
       }}
     >
       <sphereGeometry args={[0.35, 16, 16]} />
-      <meshStandardMaterial color={secret.color} emissive={secret.color} emissiveIntensity={0.6} />
+      <meshStandardMaterial
+        color={secret.color}
+        emissive={secret.color}
+        emissiveIntensity={0.6}
+      />
     </mesh>
   );
 }
 
 const StarField = memo(() => {
-  const [secrets, setSecrets] = useState<any[]>([]);
+  const secrets = useGalaxyStore(s => s.secrets);
+  const fetchSecrets = useGalaxyStore(s => s.fetchSecrets);
 
   useEffect(() => {
-    fetch('/api/secrets')
-      .then(res => res.json())
-      .then(data => setSecrets(data.secrets))
-      .catch(console.error);
-  }, []);
+    fetchSecrets();
+  }, [fetchSecrets]);
 
   return (
     <>
