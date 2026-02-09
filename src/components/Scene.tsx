e41ae48863d97caf13468564137f4d208cee97ce
perunbro <ilariamattana22@gmail.com>
2026-02-09 21:43:00 +0100

    Update Scene.tsx

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
-import { StarField } from "./StarField";
+import { GalaxyEffects } from "./GalaxyEffects"; // animazioni avanzate
 
 export const Scene: React.FC = () => {
   return (
     <Canvas camera={{ position: [0, 0, 10], fov: 75 }}>
+      {/* Luci */}
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} />
-      <Stars />
-      <StarField />
-      <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
-    </Canvas>
-  );
-};
+
+      {/* Sfondo stellato */}
+      <Stars radius={50} depth={50} count={5000} factor={4} saturation={0} fade />
+
+      {/* Stell*
