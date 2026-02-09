4cc8d3be67993f9504242d2b03816cc179eb7a68
perunbro <ilariamattana22@gmail.com>
2026-02-09 21:45:28 +0100

    Update Scene.tsx

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
-import { GalaxyEffects } from "./GalaxyEffects"; // animazioni avanzate
+import { GalaxyEffects } from "./GalaxyEffects";
 
 export const Scene: React.FC = () => {
   return (
@@ -13,4 +13,11 @@ export const Scene: React.FC = () => {
       {/* Sfondo stellato */}
       <Stars radius={50} depth={50} count={5000} factor={4} saturation={0} fade />
 
-      {/* Stell*
+      {/* Stelle animate, clusterizzate e cliccabili */}
+      <GalaxyEffects />
+
+      {/* Controlli camera */}
+      <OrbitControls enablePan enableZoom enableRotate />
+    </Canvas>
+  );
+};
